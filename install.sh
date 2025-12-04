f32550eba702de30e561c8f663b9083028569b71
lublst <246730755+lublst@users.noreply.github.com>
2025-12-04 04:34:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,31 @@
+#!/usr/bin/env bash
+
+BUNDLE=0
+
+for arg in "$@"; do
+    [ "$arg" = "--bundle" ] && BUNDLE=1
+done
+
 NAME=essential-tweaks
 DOMAIN=lublst.github.io
 ZIP_NAME=$NAME@$DOMAIN.zip
 
-echo -e ":: Creating extension bundle..."
-cd src
-zip -qr $ZIP_NAME *
+mkdir -p build
+cp -r assets build
+cp -r src/* build
+cp LICENSE build
+cp README.md build
 
-echo -e ":: Installing extension..."
-gnome-extensions install -f $ZIP_NAME
-rm -rf $ZIP_NAME
+echo -e ":: Creating extension bundle..."
+cd build
+zip -qr "$ZIP_NAME" *
+
+if [ $BUNDLE -eq 1 ]; then
+    cp "$ZIP_NAME" ..
+else
+    echo -e ":: Installing extension..."
+    gnome-extensions install -f "$ZIP_NAME"
+fi
+
+cd ..
+rm -rf build
